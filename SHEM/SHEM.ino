47d13d99b833f6cdd0cdfbf5f7f5b710e3756b95
Guandi Chen <140076679+grandy0831@users.noreply.github.com>
2023-11-30 23:18:06 +0000

    Update SHEM.ino

--- SHEM/SHEM.ino
+++ SHEM/SHEM.ino
@@ -1,83 +1,93 @@
-#include <Wire.h>  // Include Wire library for I2C communication
-#include <SparkFun_SCD30_Arduino_Library.h>  // Include library for SCD30 CO2 sensor
-#include <Adafruit_NeoPixel.h>  // Include library for controlling NeoPixels
-#include <Adafruit_GFX.h>  // Include Adafruit Graphics library for graphical functions
-#include <Adafruit_SSD1306.h>  // Include library for OLED display
+#include <Wire.h>
+#include <SparkFun_SCD30_Arduino_Library.h>
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-#define NEOPIXEL_PIN 6  // Define the pin for NeoPixels
-#define NUMPIXELS    8  // Number of pixels in the NeoPixel strip
-#define OLED_RESET   -1 // OLED reset pin (set to -1 if not used)
-
-// Initialize NeoPixel and OLED display
+#define NEOPIXEL_PIN 6
+#define NUMPIXELS    8
+#define OLED_RESET   -1
 Adafruit_NeoPixel pixels(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
-
-SCD30 airSensor; // Create an instance of the SCD30 sensor
-int mq135SensorPin = A0; // Pin for the MQ-135 air quality sensor
+SCD30 airSensor;
+int mq135SensorPin = A0;
 
 void setup() {
-  Wire.begin();  // Start the I2C communication
-  Serial.begin(9600);  // Begin serial communication at 9600 baud rate
-  pixels.begin();  // Initialize NeoPixel strip
-
-  // Initialize OLED display
+  Wire.begin();
+  Serial.begin(115200);
+  pixels.begin();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
   display.clearDisplay();
   display.setTextSize(2);  // Set text size for the OLED display
   display.setTextColor(WHITE);
 
-  // Check if SCD30 sensor is connected
   if (!airSensor.begin()) {
     Serial.println("SCD30 not detected, please check the wiring!");
     display.println("SCD30 not detected!");
     display.display();
-    while (1); // Stop execution if the sensor is not detected
+    while (1);
   }
 }
 
 void loop() {
-  // Check if there's data available from the SCD30 sensor
+  readCO2();
+  readAirQuality();
+  updateDisplay();
+  delay(2000);
+}
+
+void readCO2() {
   if (airSensor.dataAvailable()) {
-    int co2 = airSensor.getCO2(); // Get CO2 reading
-    float temperature = airSensor.getTemperature(); // Get temperature reading
-    float humidity = airSensor.getHumidity(); // Get humidity reading
-    int mq135SensorValue = analogRead(mq135SensorPin); // Read MQ-135 sensor value
-    String airQualityStatus = mq135SensorValue > 200 ? "BAD" : "GOOD"; // Determine air quality
+    int co2 = airSensor.getCO2(); // Get CO2 concentration
+
+    Serial.print("CO2(ppm): ");
+    Serial.print(co2);
+    Serial.print(" Temperature(C): ");
+    Serial.print(airSensor.getTemperature(), 1);
+    Serial.print(" Humidity(%): ");
+    Serial.println(airSensor.getHumidity(), 1);
+
+    if (co2 >= 3000) {
+        blinkRed(); // Blink red continuously if CO2 is 3000ppm or higher
+    } else {
+        setGreen(); // Keep green if CO2 is lower than 3000ppm
+    }
+  }
+}
+
+
+void readAirQuality() {
+  int mq135SensorValue = analogRead(mq135SensorPin);
+  Serial.print("Air Quality: ");
+  Serial.println(mq135SensorValue);
+}
+
+void updateDisplay() {
+  if (airSensor.dataAvailable()) {
+    float temperature = airSensor.getTemperature();
+    float humidity = airSensor.getHumidity();
+    int co2 = airSensor.getCO2();
+    int mq135SensorValue = analogRead(mq135SensorPin);
+    String airQualityStatus = mq135SensorValue > 200 ? "BAD" : "GOOD";
 
     // Update OLED display with sensor readings
     display.clearDisplay();
     display.setCursor(0,0);
     display.print("TEMP: ");
     display.print(temperature, 1);
+    display.setCursor(0, 16);
     display.print("HUM:  ");
     display.print(humidity, 1);
+    display.setCursor(0, 32);
     display.print("CO2:  ");
     display.print(co2);
+    display.setCursor(0, 48);
     display.print("AirQ: ");
     display.println(airQualityStatus);
     display.display();
-
-    // Print readings to serial monitor
-    Serial.print("CO2(ppm): ");
-    Serial.print(co2);
-    Serial.print(" Temperature(C): ");
-    Serial.print(temperature, 1);
-    Serial.print(" Humidity(%): ");
-    Serial.println(humidity, 1);
-    Serial.print("Air Quality : ");
-    Serial.println(mq135SensorValue);
-
-    // Change NeoPixel color based on CO2 level
-    if (co2 >= 3000) {
-        blinkRed();
-    } else {
-        setGreen();
-    }
   }
-  delay(2000); // Delay between readings
 }
 
-// Function to blink NeoPixels red
 void blinkRed() {
   for (int i = 0; i < NUMPIXELS; i++) {
     pixels.setPixelColor(i, pixels.Color(255, 0, 0));
@@ -89,11 +99,9 @@ void blinkRed() {
   delay(10);
 }
 
-// Function to set NeoPixels to green
 void setGreen() {
   for (int i = 0; i < NUMPIXELS; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 255, 0));
   }
   pixels.show();
 }
-
